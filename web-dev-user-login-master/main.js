d6c861f96bb792913782ab9554ca7b47d710cc4e
Jaedyn <61067221+Jcarbonell04@users.noreply.github.com>
2020-04-29 17:02:31 -0600

    Update main.js

--- web-dev-user-login-master/main.js
+++ web-dev-user-login-master/main.js
@@ -5,21 +5,19 @@ document.getElementById("loginBtn").addEventListener("click", login);
 
 // Event Functions
 function login() {
-  // Input  
+  // Input
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
-  username = toLowerCase();
 
-    // Sign In Successful
-    if (username == "Jcarbonell04" && password == "HarryAinlayHS") {
+  // Sign In Successful
+  if (username == "Jcarbonell04" && password == "HarryAinlayHS") {
     alert("Sign In Successful");
 
     // Sign In Unsuccessful
-    } else {
+  } else {
     alert("Sign In Unsuccessful");
-    document.getElementById("output").innerHTML ="Password Unsucesssful/Try Again";
+    document.getElementById("output").innerHTML = "Wrong Password, Try Again!";
     document.getElementById("output").style.color = "red";
-    
-    }
+    document.getElementById("output").style.fontWeight = "bold";
+  }
 }
-
